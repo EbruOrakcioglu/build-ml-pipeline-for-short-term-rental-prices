583ca845eee1107cf18ba109970fc8a1df7d833f
eorakcioglu <ebru.orakcioglu@nesine.com>
2026-02-13 09:50:24 +0300

    Update all changes

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+"""
+Performs basic cleaning on the data and saves the results in W&B
+"""
+import argparse
+import logging
+import pandas as pd
+import wandb
+import os
+
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(project="nyc_airbnb",
+                     job_type="basic_cleaning")
+    run.config.update(args)
+
+
+    artifact_path = run.use_artifact(args.input_artifact).file()
+    print("Selam artifact_path {artifact_path} burada")
+    df = pd.read_csv(artifact_path)
+
+    min_price = args.min_price
+    max_price = args.max_price
+    
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This step cleans the data")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type= str,
+        help= "Third Parameter",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type= str,
+        help= "Third Parameter",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type= str,
+        help= "Third Parameter",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type= str,
+        help= "Third Parameter",
+        required=True
+    )
+    parser.add_argument(
+        "--min_price", 
+        type= float,
+        help= "First Parameter",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type= float,
+        help= "Second Parameter",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -54,7 +54,7 @@ def go(args):
     ######################################
     # Use run.use_artifact(...).file() to get the train and validation artifact (args.trainval_artifact)
     # and save the returned path in train_local_path
-    trainval_local_path = # YOUR CODE HERE
+    trainval_local_path = run.use_artifact(args.trainval_artifact).file()
     ######################################
 
     X = pd.read_csv(trainval_local_path)
@@ -76,13 +76,14 @@ def go(args):
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+    sk_pipe.fit(X_train[processed_features], y_train)
     ######################################
 
     # Compute r2 and MAE
     logger.info("Scoring")
-    r_squared = sk_pipe.score(X_val, y_val)
+    r_squared = sk_pipe.score(X_val[processed_features], y_val)
 
-    y_pred = sk_pipe.predict(X_val)
+    y_pred = sk_pipe.predict(X_val[processed_features])
     mae = mean_absolute_error(y_val, y_pred)
 
     logger.info(f"Score: {r_squared}")
@@ -98,6 +99,13 @@ def go(args):
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
     # YOUR CODE HERE
+    mlflow.sklearn.save_model(
+            sk_pipe,
+            "random_forest_dir",
+            serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+            input_example=X_val.iloc[:2],
+        )
+
     ######################################
 
     ######################################
@@ -107,6 +115,16 @@ def go(args):
     # you just created to add the "random_forest_dir" directory to the artifact, and finally use
     # run.log_artifact to log the artifact to the run
     # YOUR CODE HERE
+
+    artifact = wandb.Artifact(
+            args.output_artifact,
+            type="model_export",
+            metadata=rf_config,
+            description="Random Forest pipeline export",
+    )
+    artifact.add_dir("random_forest_dir")
+
+    run.log_artifact(artifact)
     ######################################
 
     # Plot feature importance
@@ -117,6 +135,7 @@ def go(args):
     run.summary['r2'] = r_squared
     # Now log the variable "mae" under the key "mae".
     # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -156,7 +175,9 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # Build a pipeline with two steps:
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
-    non_ordinal_categorical_preproc = # YOUR CODE HERE
+    non_ordinal_categorical_preproc =  make_pipeline(
+        SimpleImputer(strategy="most_frequent"), OneHotEncoder()
+    )
     ######################################
 
     # Let's impute the numerical columns to make sure we can handle missing values
@@ -182,7 +203,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     )
 
     # Some minimal NLP for the "name" column
-    reshape_to_1d = FunctionTransformer(np.reshape, kw_args={"newshape": -1})
+    reshape_to_1d = FunctionTransformer(np.reshape, kw_args={"shape": -1})
     name_tfidf = make_pipeline(
         SimpleImputer(strategy="constant", fill_value=""),
         reshape_to_1d,
@@ -215,7 +236,12 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # ColumnTransformer instance that we saved in the `preprocessor` variable, and a step called "random_forest"
     # with the random forest instance that we just saved in the `random_forest` variable.
     # HINT: Use the explicit Pipeline constructor so you can assign the names to the steps, do not use make_pipeline
-    sk_pipe = # YOUR CODE HERE
+    sk_pipe = pipe = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("random_forest", random_forest),
+        ]
+    )
 
     return sk_pipe, processed_features
 
